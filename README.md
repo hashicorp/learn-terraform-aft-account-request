3061cf3a00fc0c0e537a679ccf506da03ece7a31
ritsok <msokolova13@gmail.com>
2021-11-23 10:46:41 -0500

    Account request config

---
+++ README.md
@@ -0,0 +1,11 @@
+# Learn Terraform - Use Control Tower Account Factory for Terraform
+
+This is a companion repository for the [Provision and Manage Accounts with
+Control Tower Account Factory for Terraform
+tutorial](https://learn.hashicorp.com/tutorials/terraform/aws-control-tower-aft)
+tutorial on HashiCorp Learn.
+
+This repository contains boilerplate configuration for defining account
+requests to use with the Account Factory for Terraform module. THe AFT pipeline
+requires the structure used and the included submodule to define a DynamoDB
+table item with your account specifications.
---
+++ terraform/main.tf
@@ -0,0 +1,27 @@
+module "sandbox" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "<ACCOUNT EMAIL>"
+    AccountName               = "sandbox-aft"
+    ManagedOrganizationalUnit = "Learn AFT"
+    SSOUserEmail              = "<SSO EMAIL>"
+    SSOUserFirstName          = "Sandbox"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
