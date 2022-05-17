d5b3726cc1af8c5170ac28e1c0f4d0977f50a5ee
romarioschneider <schneidersoft@ukr.net>
2022-05-17 22:09:03 +0300

    add fisrt AFT-pipeline account

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,55 @@
-module "sandbox" {
+//module "sandbox" {
+//  source = "./modules/aft-account-request"
+//
+//  control_tower_parameters = {
+//    AccountEmail              = "<ACCOUNT EMAIL>"
+//    AccountName               = "sandbox-aft"
+//    ManagedOrganizationalUnit = "Learn AFT"
+//    SSOUserEmail              = "<SSO EMAIL>"
+//    SSOUserFirstName          = "Sandbox"
+//    SSOUserLastName           = "AFT"
+//  }
+//
+//  account_tags = {
+//    "Learn Tutorial" = "AFT"
+//  }
+//
+//  change_management_parameters = {
+//    change_requested_by = "HashiCorp Learn"
+//    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+//  }
+//
+//  custom_fields = {
+//    group = "non-prod"
+//  }
+//
+//  account_customizations_name = "sandbox"
+//}
+
+module "first_acc" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
-    AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
-    SSOUserFirstName          = "Sandbox"
+    AccountEmail              = "schneidersoft+b@ukr.net"
+    AccountName               = "first-aft"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "schneidersoft+b@ukr.net"
+    SSOUserFirstName          = "First"
     SSOUserLastName           = "AFT"
   }
 
   account_tags = {
-    "Learn Tutorial" = "AFT"
+    "Name" = "First AFT"
   }
 
   change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+    change_requested_by = "first terraform apply"
+    change_reason       = "first testing"
   }
 
   custom_fields = {
     group = "non-prod"
   }
 
-  account_customizations_name = "sandbox"
+  account_customizations_name = "first"
 }
