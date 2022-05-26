10b6645b7d76ad8a8025cb79e7f0f42bc09dd5af
Olekidh <50212100+Olekidh@users.noreply.github.com>
2022-05-26 11:47:43 +0200

    Added first AFT account

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
-    AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    AccountEmail              = "afttest2022@gmail.com"
+    AccountName               = "spaceQueue"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "afttest2022@gmail.com"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
