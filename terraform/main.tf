eba24b260eb859b8aa64d06d404049cd4fa8b9d7
Harry M. Winters <harry.winters@tunetx.com>
2022-10-17 13:22:20 -0700

    Add harry winters sandbox account and pre-commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,24 @@
-module "sandbox" {
+module "harry-winters-sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
-    AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "AFT"
+    AccountEmail              = "harry.winters+personal_sandbox@tunetx.com"
+    AccountName               = "harry-winters-sandbox"
+    ManagedOrganizationalUnit = "Bioinformatics-0"
+    SSOUserEmail              = "harry.winters+personal_sandbox@tunetx.com"
+    SSOUserFirstName          = "Harry"
+    SSOUserLastName           = "Winters"
   }
 
   account_tags = {
-    "Learn Tutorial" = "AFT"
+    "workload" = "sandbox"
   }
 
   change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+    change_requested_by = "Harry Winters"
+    change_reason       = "Initial account provisioning."
   }
 
-  custom_fields = {
-    group = "non-prod"
-  }
+  custom_fields = {}
 
-  account_customizations_name = "sandbox"
 }
