36a64e116c7160bb0bfb2aebf9c77a329015f8a9
Nicholas Arvelo <info@nicholasarvelo.com>
2023-05-23 10:33:09 -0400

    Create sandbox-aft Control Tower account

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
+    AccountEmail              = "nicholas+5085cbd2@hustle.life"
     AccountName               = "sandbox-aft"
     ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    SSOUserEmail              = "nicholas+5085cbd2@hustle.life"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
