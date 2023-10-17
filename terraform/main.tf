da095c3f260ddb0eff3040fb5bc5ab60dc220847
Alex Mazur <alexmazur@MacBook-Pro-de-Alex.local>
2023-10-17 18:49:34 -0300

    Deploy Sandbox

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
+    AccountEmail              = "alexmazur.dev5+sandbox@outlook.com"
     AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    ManagedOrganizationalUnit = "Products"
+    SSOUserEmail              = "alexmazur.dev5+sandbox@outlook.com"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
