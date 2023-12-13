98e395170337e294ab7fb680ef39e2bf71c1e9f9
Raj <rvaddesubus@gmail.com>
2023-12-13 16:19:39 -0500

    testing with vend 121301

---
+++ terraform/aftvend001.tf
@@ -0,0 +1,29 @@
+module "aftvend001" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "rvadde+ymus-vend121301@skepticservices.com"
+    AccountName               = "AFT_Vend_121301"
+    ManagedOrganizationalUnit = "ou-ty2s-ljetjmp9"
+    SSOUserEmail              = "rajesh.vadde@trace3.com"
+    SSOUserFirstName          = "Raj"
+    SSOUserLastName           = "V"
+  }
+
+  account_tags = {
+    "Environment" = "Workloads-NonProd"
+    "Company" = "YMUS"
+    "Business Unit" = "Applications"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Trace3"
+    change_reason       = "Testing the AFT Account Vending"
+  }
+
+  custom_fields = {
+    group = "Non-Prod"
+  }
+
+  account_customizations_name = "Workloads_NonProd"
+}
